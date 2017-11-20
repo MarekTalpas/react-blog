08232c3b166bf6624eca112d5112f16a70f856ab
marek.talpas <marek.talpas@globallogic.com>
2017-11-20 22:22:35 +0100

    initialized react-blog app

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { startLogout } from '../actions/auth';
+
+export const Header = ({ startLogout }) => (
+  <header className="header">
+    <div className="content-container">
+      <div className="header__content">
+        <Link className="header__title" to="/dashboard">
+          <h1>Boilerplate</h1>
+        </Link>
+        <button className="button button--link" onClick={startLogout}>Logout</button>
+      </div>
+    </div>
+  </header>
+);
+
+const mapDispatchToProps = dispatch => ({
+  startLogout: () => dispatch(startLogout())
+});
+
+Header.defaultProps = {
+  startLogout: () => {}
+};
+
+Header.propTypes = {
+  startLogout: PropTypes.func
+};
+
+export default connect(undefined, mapDispatchToProps)(Header);
---
+++ src/routers/PrivateRoute.js
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { connect } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+import Header from '../components/Header';
+
+export const PrivateRoute = ({
+  isAuthenticated,
+  component: Component,
+  ...rest
+}) => (
+  <Route
+    {...rest}
+    component={props => (isAuthenticated
+    ? (
+      <div>
+        <Header />
+        <Component {...props} />
+      </div>
+    )
+    : (<Redirect to="/" />))}
+  />
+);
+
+const mapStateToProps = state => ({
+  isAuthenticated: !!state.auth.uid
+});
+
+PrivateRoute.defaultProps = {
+  isAuthenticated: false,
+  component: () => {}
+};
+
+PrivateRoute.propTypes = {
+  isAuthenticated: PropTypes.bool,
+  component: PropTypes.func
+};
+
+export default connect(mapStateToProps)(PrivateRoute);
