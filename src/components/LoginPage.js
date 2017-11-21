e28297679177fc40e0a7ef26c328a08d636f9701
marek.talpas <marek.talpas@globallogic.com>
2017-11-21 21:27:17 +0100

    added facebook login and styles to loginPage

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { startLogin } from '../actions/auth';
+import { startLoginWithGoogle, startLoginWithFacebook } from '../actions/auth';
 
-export const LoginPage = ({ startLogin }) => (
+export const LoginPage = ({ startLoginWithGoogle, startLoginWithFacebook }) => (
   <div className="box-layout">
     <div className="box-layout__box">
-      <h1 className="box-layout__title">Boilerplate</h1>
-      <p>Tag line for app.</p>
-      <button className="button" onClick={startLogin}>Login with Google</button>
+      <h1 className="box-layout__title">React Blog</h1>
+      <p className="box-layout__subtitle">Best Blog ever.</p>
+      <button className="button google" onClick={startLoginWithGoogle}>Login with Google</button>
+      <button className="button facebook" onClick={startLoginWithFacebook}>Login with Facebook</button>
     </div>
   </div>
 );
 
-const mapDispatchToProps = (dispatch) => ({
-  startLogin: () => dispatch(startLogin())
+LoginPage.defaultProps = {
+  startLoginWithGoogle: () => {},
+  startLoginWithFacebook: () => {}
+};
+
+LoginPage.propTypes = {
+  startLoginWithGoogle: PropTypes.func,
+  startLoginWithFacebook: PropTypes.func
+};
+
+const mapDispatchToProps = dispatch => ({
+  startLoginWithGoogle: () => dispatch(startLoginWithGoogle()),
+  startLoginWithFacebook: () => dispatch(startLoginWithFacebook())
 });
 
 export default connect(undefined, mapDispatchToProps)(LoginPage);
